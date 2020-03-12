49a652d6ca099425c377cd7672c4eb582117b0a6
albertdong2020 <60301242+albertdong2020@users.noreply.github.com>
2020-03-12 17:00:58 -0400

    Add files via upload
    CameraSelect command

---
+++ src/main/java/frc/robot/commands/CameraSelect.java
@@ -0,0 +1,50 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class CameraSelect extends CommandBase {
+  /**
+   * Creates a new VisionActivate.
+   */
+  String[] modes = { "intake", "shooter", "arm"};
+
+  public VisionActivate() {
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+      
+      //SmartDashboard.putNumber("Distance", SmartDashboard.getNumberArray("vision/target_info", new double[]{0,0,0,0,0,0,0})[3]);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+      var mode = SmartDashboard.getString("/vision/active_mode", "intake");
+      var idx = java.util.Arrays.asList(modes).indexOf(mode);
+      if (idx<0) idx =0;
+      idx = (idx + 1 ) % 3;
+      SmartDashboard.putString("/vision/active_mode", modes[idx]);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+}
